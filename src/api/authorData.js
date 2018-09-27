8fb43e590fb77cc1187c9e0104b08893e376696a
Coreeze <cristianlenta@yahoo.com>
2018-09-27 21:11:43 +0200

    first commit

---
+++ src/api/authorData.js
@@ -0,0 +1,21 @@
+module.exports = {
+	authors: 
+	[
+			
+		{
+			id: 'cris-l', 
+			firstName: 'Cris', 
+			lastName: 'L.'
+		},
+		{
+			id: 'cory-house', 
+			firstName: 'Cory', 
+			lastName: 'House'
+		},	
+		{
+			id: 'the-other-guy', 
+			firstName: 'The Other', 
+			lastName: 'Guy'
+		}
+	]
+};
